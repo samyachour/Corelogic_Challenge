eaa43f987209781439c313044a84d7468969a0d8
samy achour <sachour96@gmail.com>
2017-01-07 20:36:46 -0800

    did some stuff

--- houseData.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  7 00:30:52 2017
-
-@author: samy
-"""
-
---
+++ parcelData.py
@@ -0,0 +1,9 @@
+import pandas
+import shapefile
+
+def getParcel():
+    df = pandas.read_csv("../CorelogicResources/Corelogic_houses_csv.csv")
+    sf = shapefile.Reader("../CorelogicResources/Parcels/PARCELS.shp")
+    print(len(sf.records()))
+    
+getParcel()

968bf7016cdfb5e39b9834af9817e020f458ae09
samy achour <sachour96@gmail.com>
2017-01-13 17:41:10 -0800

    got python to work with shapely types from geom

--- pythonShapefilePostGIS.py
+++ pythonShapefilePostGIS.py
@@ -1,30 +1,67 @@
 from sqlalchemy import create_engine
 import pandas as pd
 from shapely import geometry
+from shapely import wkt
 from matplotlib import pyplot as plt
+from descartes.patch import PolygonPatch
+from figures import BLUE, SIZE, set_limits, plot_coords, color_isvalid
+# TODO: check for xy coords, exclude multi's?
 
-engine = create_engine('postgresql://@localhost:5432/parcels')
-sql = """
-SELECT *, ST_AsText(geom) FROM public.parcelterminal WHERE "gid" < 5;
-"""
-parcels = pd.read_sql(sql, engine)
-print(parcels['st_astext'].iloc[0])
+def getPolygon(geomCol, table, idCol, rowNum):
+    engine = create_engine('postgresql://@localhost:5432/parcels')
+    sql = 'SELECT *, ST_AsText({}) FROM {} WHERE "{}" < {};'.format(geomCol, table, idCol, rowNum + 5)
+    data = pd.read_sql(sql, engine)
+    #print(data['st_astext'].iloc[rowNum])
+    
+    
+    shape = wkt.loads(data['st_astext'].iloc[rowNum])
+        
+    fig = plt.figure(1, figsize=SIZE, dpi=90)
+    ax = fig.add_subplot(121)
+    for polygon in shape:
+        plot_coords(ax, polygon.exterior)
+        patch = PolygonPatch(polygon, facecolor=color_isvalid(shape), edgecolor=color_isvalid(shape, valid=BLUE), alpha=0.5, zorder=2)
+        ax.add_patch(patch)
+        
+    ax.set_title('a) valid')
+        
+        
+
+getPolygon("geom", "public.parcelterminal", "gid", 1)
+#getPolygon("geom", "public.topographical", "id", 0)
 
 
 
-inputString = parcels['st_astext'].iloc[1][15:-3]
-inputString = inputString.split(",")
-points = []
-for coord in inputString:
-    coords = geometry.Point(float(coord.split(" ")[0]), float(coord.split(" ")[1]))
-    points.append(coords)
 
-poly = geometry.Polygon([[p.x, p.y] for p in points])
-print(poly)
 
-x,y = poly.exterior.xy
-fig = plt.figure(1, figsize=(5,5), dpi=90)
-ax = fig.add_subplot(111)
-ax.plot(x, y, color='#6699cc', alpha=0.7,
-    linewidth=3, solid_capstyle='round', zorder=2)
-ax.set_title('Polygon')
+
+def test(rowNum=0):
+    engine = create_engine('postgresql://@localhost:5432/parcels')
+    #sql = 'SELECT *, ST_AsText("geom") FROM public.parcelterminal WHERE "multi" = \'Y\';'
+    sql = 'SELECT *, ST_AsText("geom") FROM public.parcelterminal WHERE "gid" < 7'
+    data = pd.read_sql(sql, engine)
+    print(type(wkt.loads(data['st_astext'].iloc[rowNum])))
+    
+    '''
+    print(data['st_astext'].iloc[rowNum])
+    
+    inputString = data['st_astext'].iloc[rowNum][15:-3]
+    inputString = inputString.split(",")
+    points = []
+    for coord in inputString:
+        coords = geometry.Point(float(coord.split(" ")[0]), float(coord.split(" ")[1]))
+        points.append(coords)
+    
+    poly = geometry.Polygon([[p.x, p.y] for p in points])
+    
+    x,y = poly.exterior.xy
+    fig = plt.figure(1, figsize=(5,5), dpi=90)
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, color='#6699cc', alpha=0.7,
+        linewidth=3, solid_capstyle='round', zorder=2)
+    ax.set_title('Polygon')
+    
+    print(data['total_lvg_'].iloc[rowNum])
+    '''
+    
+#test(0)
